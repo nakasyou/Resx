a661904fe05d04cf35d106dd1f934427b5a0f8fe
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-12 17:33:01 +0900

    Create html.ts

---
+++ responses/html.ts
@@ -0,0 +1,34 @@
+import { deepMerge } from "../deps.ts"
+
+export type HtmlInput = string
+
+const defaultResponseOption = {
+  headers: {
+    "content-type": "text/html",
+  }
+}
+
+/**
+ * Returns HTML response.
+ * @example
+ * ```ts
+ * import * as resx from "https://deno.land/x/resx"
+ * 
+ * const response: Response = resx.html(`
+ *   <!doctype HTML>
+ *   <html>
+ *     <head>
+ *        <!-- Head content here -->
+ *     </head>
+ *     <body>
+ *       <h1>Hello World!!</h1>
+ *     </body>
+ *   </html>
+ * `)
+ * ```
+ */
+export function html(input: JsonInput, responseOption): Response { 
+  const newResponseOption = deepMerge.deepMerge(defaultResponseOption, responseOption) // Merge response constructor option.
+
+  return new Reponse(input, newResponseOption)
+}
