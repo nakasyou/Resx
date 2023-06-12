e41f8376d2940b26fff0f91f6e34b3a0628de7c3
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-12 17:25:29 +0900

    Create json.ts

---
+++ responses/json.ts
@@ -0,0 +1,20 @@
+import { deepMerge } from "../deps.ts"
+
+export type JsonInput = any
+
+const defaultResponseOption = {
+  headers: {
+    "content-type": "application/json",
+  }
+}
+
+/**
+ * Returns JSON response.
+ */
+export function json(input: JsonInput, responseOption): Response {
+  const json = JSON.stringify(input)
+  
+  const newResponseOption = deepMerge.deepMerge(defaultResponseOption, responseOption) // Merge response constructor option.
+
+  return new Reponse(json, newResponseOption)
+}
